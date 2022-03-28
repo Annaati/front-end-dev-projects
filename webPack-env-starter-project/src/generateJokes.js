bfe2a6f6ed3297c913abd20d23b26d8ef72bae89
Abdifatah Annaati <66587544+Annaati@users.noreply.github.com>
2022-03-28 22:12:41 +0300

    webpack env dev starter

---
+++ webPack-env-starter-project/src/generateJokes.js
@@ -0,0 +1,5 @@
+function generateJoke(){
+  return "IWhy couldn't the lifeguard save the hippie? He was too far out, man."
+}
+
+export default generateJoke
--- webPack-env-starter-project/src/index.js
+++ webPack-env-starter-project/src/index.js
@@ -1,1 +1,8 @@
-console.log("WebPack Enviroment For Front-EndDev Starter Projects")
+import generateJoke from './generateJokes';
+import './styles/main.scss';
+import laughing from './assets/laughing.svg'
+
+const LaughImg = document.getElementById('laughImg')
+LaughImg.src = laughing
+
+console.log(generateJoke());
